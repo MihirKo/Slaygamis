90f8143355edd16c5c67298bbd7996c9f0abf1d8
MihirKolte <58711408+MihirKo@users.noreply.github.com>
2022-04-07 14:59:29 -0500

    Initialized variables, added servo spin function

--- SpookyCave.ino
+++ SpookyCave.ino
@@ -6,12 +6,28 @@
  * Description: Bumper and Servo program
  */
 
+ //Servo code library
+ #include<Servo.h>;
+
+ //VARIABLES
+ Servo myServo;
+ const int buttonPin =7;
+
 void setup() {
   // put your setup code here, to run once:
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //if button is pressed, swivel servo 180 degress
+  if(digitalRead(buttonPin)==HIGH){
 
+  myServo.write(180);
+  }
+  else{
+    myServo.write(0);
+  }
 }
